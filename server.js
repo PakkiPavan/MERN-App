0acfc3e0fa264f99b94bb0fa209c48ce0e6016d4
harish patnaik <pavanpakki786@gmail.com>
2019-02-17 20:31:07 +0530

    'chang'

--- server.js
+++ server.js
@@ -46,24 +46,23 @@ if(process.env.NODE_ENV==='production')
 	})
 
 	app.post("/inc",function(req,res){
-		var count;
-		console.log("Inside inc")
-		console.log(req.body)	
-		mongoose.connect(url,function(err,db){
+	var count;
+	mongoose.connect(url,function(err,db){
+		if(err) throw err;
+		db.collection('collectionTest1').find({_id:"003"}).toArray(function(err,docs){
 			if(err) throw err;
-			db.collection('collectionTest1').find({_id:"003"}).toArray(function(err,docs){
+			count=docs[0].count;
+			intCount=parseInt(req.body.count)+1
+			db.collection('collectionTest1').update({count:count},{$set:{"count":intCount}},function(err,res){
 				if(err) throw err;
-				//res.json(docs);
-				count=docs[0].count;
-				//console.log(count)
-				intCount=parseInt(req.body.count)+1
-				db.collection('collectionTest1').updateOne({count:count},{$set:{"count":intCount}})
-				res.send("Incremented"+" "+docs+" "+count+" "+intCount);
 				console.log("Incremented")
+				db.close();
 			})
-			db.close();
+			res.send("Incremented"+" "+JSON.stringify(docs)+" "+count+" "+intCount);
+			})
 		})	
 	});
+
 	
 
 }
@@ -74,7 +73,7 @@ if(process.env.NODE_ENV==='production')
 })
 */
 
-app.get("/api",function(req,res){
+app.use("/api",function(req,res){
 	var count;
 	//console.log(req.body)
 	mongoose.connect(url,function(err,db){
@@ -107,11 +106,13 @@ app.post("/inc",function(req,res){
 			count=docs[0].count;
 			//console.log(count)
 			intCount=parseInt(req.body.count)+1
-			db.collection('collectionTest1').update({count:count},{$set:{"count":intCount}})
-			res.send("Incremented");
-			console.log("Incremented")
+			db.collection('collectionTest1').update({count:count},{$set:{"count":intCount}},function(err,res){
+				if(err) throw err;
+				console.log("Incremented")
+				db.close();
+			})
+			res.send("Incremented"+" "+JSON.stringify(docs)+" "+count+" "+intCount);
 		})
-		db.close();
 	})	
 });
 
